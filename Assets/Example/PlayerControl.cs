c1ed2ac18a6d8389d0990ce7c54e9754d63c9077
yumi nunoura <uranuno@gmail.com>
2015-05-01 19:25:58 +0900

    Initial Commit

---
+++ Assets/Example/PlayerControl.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed;
+	public float turnSmooth;
+
+	new Rigidbody rigidbody;
+	new Transform transform;
+
+	Vector2 m_lookTarget;
+
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+		transform = base.transform;
+	}
+
+	void Update () {
+		// 動く入力
+		Vector2 moveInput = new Vector2 (
+			CrossPlatformInputManager.GetAxisRaw ("Horizontal"),
+			CrossPlatformInputManager.GetAxisRaw ("Vertical")
+		);
+
+		// 動く方向をみる
+		m_lookTarget = moveInput;
+
+		// 動く方向があるとき
+		if (m_lookTarget != Vector2.zero) {
+			// 向きを更新
+			transform.rotation = Quaternion.Lerp(
+				transform.rotation,
+				Quaternion.LookRotation (new Vector3 (m_lookTarget.x, 0, m_lookTarget.y)),
+				turnSmooth * Time.deltaTime
+			);
+		}
+	}
+
+	void FixedUpdate () {
+		// 動く方向があるときだけ進む
+		if (m_lookTarget != Vector2.zero) {
+			Vector3 movement = new Vector3(m_lookTarget.x, 0, m_lookTarget.y);
+			rigidbody.velocity = new Vector3 (
+				movement.x * speed,
+				rigidbody.velocity.y,//Y軸は速度をいじらない
+				movement.z * speed
+			);
+		}
+	}
+}
---
+++ Assets/MovableJoystick/MovableJoystick.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class MovableJoystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler {
+
+	[SerializeField]
+	RectTransform stickObject;
+
+	[SerializeField]
+	RectTransform baseObject;
+
+	public float radius = 100f;
+
+	public string horizontalAxisName = "Horizontal";
+	public string verticalAxisName = "Vertical";
+
+	CrossPlatformInputManager.VirtualAxis horizontalVirtualAxis;
+	CrossPlatformInputManager.VirtualAxis verticalVirtualAxis;
+
+	Vector2 startPos;
+
+	void OnEnable () {
+		horizontalVirtualAxis = new CrossPlatformInputManager.VirtualAxis(horizontalAxisName);
+		verticalVirtualAxis = new CrossPlatformInputManager.VirtualAxis(verticalAxisName);
+
+		CrossPlatformInputManager.RegisterVirtualAxis(horizontalVirtualAxis);
+		CrossPlatformInputManager.RegisterVirtualAxis(verticalVirtualAxis);
+
+		// 可動範囲 = 基盤の大きさ
+		if (baseObject != null) baseObject.sizeDelta = Vector2.one * radius * 2;
+
+		// 一旦(0,0)へ
+		MoveTo(Vector2.zero);
+	}
+
+	public void OnPointerDown (PointerEventData data) {
+		MoveTo (data.position);
+		OnDrag (data);
+	}
+
+	void MoveTo (Vector2 pos) {
+		startPos = pos;
+		stickObject.position = pos;
+		if (baseObject != null) baseObject.position = pos;
+	}
+
+	public void OnPointerUp (PointerEventData data) {
+		stickObject.position = startPos;
+		UpdateVirtualAxes (Vector2.zero);
+	}
+
+	public void OnDrag (PointerEventData data) {
+		Vector2 value = data.position - startPos;
+		UpdateVirtualAxes (value);
+	}
+
+	void UpdateVirtualAxes (Vector2 value) {
+		// 移動位置を指定の半径内で制限する
+		value = Vector2.ClampMagnitude (value, radius);
+
+		stickObject.position = startPos + value;
+
+		Vector2 normalizedValue = value / radius;
+		horizontalVirtualAxis.Update (normalizedValue.x);
+		verticalVirtualAxis.Update (normalizedValue.y);
+	}
+
+	void OnDisable () {
+		if (horizontalVirtualAxis != null) {
+			horizontalVirtualAxis.Remove ();
+		}
+		if (verticalVirtualAxis != null) {
+			verticalVirtualAxis.Remove ();
+		}
+	}
+}
